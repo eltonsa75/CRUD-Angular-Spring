01df219e08e86a039cca259d22e9789f9b20ec4e
Elton Andrade <eltonsa75@hotmail.com>
2022-05-06 10:43:35 -0300

    Listar Cursos (API HTTP GET)

--- crud-spring/src/main/java/com/elton/controller/CourseController.java
+++ crud-spring/src/main/java/com/elton/controller/CourseController.java
@@ -1,17 +1,18 @@
 package com.elton.controller;
 
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/hello")
-public class HelloController {
-   
-   
-    @GetMapping   
-    public  String hello() {
-        return "Hello World";
+@RequestMapping("/api/courses")
+public class CourseController {
+    
+    @GetMapping
+    public List<Object> list() {
+        return null;
     }
 }
