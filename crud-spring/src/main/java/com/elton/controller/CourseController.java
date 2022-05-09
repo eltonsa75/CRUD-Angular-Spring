9a04a1434c3c2f8688de1fb627d783ebc32e1986
Elton Andrade <eltonsa75@hotmail.com>
2022-05-09 13:58:30 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)

--- crud-spring/src/main/java/com/elton/controller/CourseController.java
+++ crud-spring/src/main/java/com/elton/controller/CourseController.java
@@ -6,6 +6,8 @@ import com.elton.model.Course;
 import com.elton.repository.CourseRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,17 @@ public class CourseController {
     private  final CourseRepository courseRepository;
 
 
-
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+        
+    }
+   
 }
