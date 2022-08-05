23a3a941e217be98931bda221916f822c367a678
Elton <eltonsa75@hotmail.com>
2022-08-05 14:11:39 -0300

    atualizando o Angular para versão 14

--- crud-spring/src/main/java/com/elton/controller/CourseController.java
+++ crud-spring/src/main/java/com/elton/controller/CourseController.java
@@ -6,7 +6,6 @@ import com.elton.model.Course;
 import com.elton.repository.CourseRepository;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
