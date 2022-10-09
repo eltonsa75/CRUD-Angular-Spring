4970c88fabe7c951310b203328c89ed6058f3719
Elton <eltonsa75@hotmail.com>
2022-10-09 11:03:04 -0300

    Salvando Formulario

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -20,15 +20,31 @@ export class CoursesService {
     .pipe(
       first(),
       // delay(5000),
-      tap(courses => console.log(courses))
+      // tap(courses => console.log(courses))
     );
   }
 
   loadById(id: string){
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
+  
 
   save(record: Partial<Course>) {
+    // console.log(record);
+    if(record._id){
+      // console.log('update');
+      return this.update(record);
+    }
+    // console.log('create');
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
 }
+
