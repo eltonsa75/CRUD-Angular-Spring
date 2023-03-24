9ce272a1ceac090551466c84027ac11f7e945fb2
Elton Andrade <elton.andrade@montreal.com.br>
2023-03-24 13:36:12 -0300

    validando o back-end

--- crud-spring/src/main/java/com/elton/model/Course.java
+++ crud-spring/src/main/java/com/elton/model/Course.java
@@ -5,6 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -20,11 +25,16 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    
+
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100) 
     @Column(length = 100, nullable = false)
     private String name;
 
-    
+    @NotBlank
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
     
