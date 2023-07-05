57e2f369c392c5c9dceaf1484dc851058211d059
Elton Andrade <eltonsa75@hotmail.com>
2023-07-05 10:28:04 -0300

    Criando a classe Lesson da aula 45: Curso-Aulas: OneToMany

---
+++ crud-spring/src/main/java/com/elton/model/Lesson.java
@@ -0,0 +1,20 @@
+package com.elton.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 11, nullable = false)
+    private String youtubeUrl;
+}
